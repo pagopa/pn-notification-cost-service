cba6ee5ace3db1ed4ac883604321861a2287eed6
ViVeniero <vincenzo.veniero@pagopa.it>
2026-03-05 14:56:24 +0100

    testing codebuild

--- functions/populateNotificationDeliveryCost/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './src/config/.env') });
-
-const { runMigration } = require("./src/app/Migration.js");
-
-
-const iunsToProcess = process.argv.slice(2);
-runMigration(iunsToProcess);
