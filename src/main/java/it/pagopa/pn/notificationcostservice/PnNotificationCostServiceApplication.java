bf98790ee5865735d4edb250f628ed9015ef6156
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-04-08 10:19:22 +0200

    [PN-19288] feat: fix

--- src/main/java/it/pagopa/pn/notificationcostservice/PnNotificationCostServiceApplication.java
+++ src/main/java/it/pagopa/pn/notificationcostservice/PnNotificationCostServiceApplication.java
@@ -3,6 +3,8 @@ package it.pagopa.pn.notificationcostservice;
 import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PnNotificationCostServiceApplication {
@@ -13,5 +15,14 @@ public class PnNotificationCostServiceApplication {
         SpringApplication app = new SpringApplication(PnNotificationCostServiceApplication.class);
         app.addListeners(new TaskIdApplicationListener());
         return app;
+    }
+
+    @RestController
+    public static class HomeController {
+
+        @GetMapping("")
+        public String home() {
+            return "Sono Vivo";
+        }
     }
 }
